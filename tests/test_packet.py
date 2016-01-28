cca8e4513ca4b0a7d01b8a21ebac8a4143d7d2c3
Kimmo Huoman <kipenroskaposti@gmail.com>
2016-01-28 20:34:31 +0200

    Separate Packet parsing and creation tests.

--- tests/test_packet.py
+++ tests/test_packet.py
@@ -2,7 +2,7 @@
 from __future__ import print_function, unicode_literals, division
 
 from enocean.protocol.packet import Packet
-from enocean.protocol.constants import PACKET, PARSE_RESULT, RORG
+from enocean.protocol.constants import PACKET, PARSE_RESULT
 
 
 def test_packet_examples():
@@ -153,72 +153,3 @@ def test_packet_fails():
     for msg in fail_examples:
         status, remainder, p = Packet.parse_msg(msg)
         assert status in [PARSE_RESULT.INCOMPLETE, PARSE_RESULT.CRC_MISMATCH]
-
-
-def test_packet_assembly():
-    PACKET_CONTENT_1 = bytearray([
-        0x55,
-        0x00, 0x0A, 0x00, 0x01,
-        0x80,
-        0xA5, 0x00, 0x00, 0x00, 0x00, 0xDE, 0xAD, 0xBE, 0xEF, 0x00,
-        0x18
-    ])
-    PACKET_CONTENT_2 = bytearray([
-        0x55,
-        0x00, 0x0A, 0x07, 0x01,
-        0xEB,
-        0xA5, 0x00, 0x00, 0x00, 0x00, 0xDE, 0xAD, 0xBE, 0xEF, 0x00,
-        0x03, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x00,
-        0xE4
-    ])
-    PACKET_CONTENT_3 = bytearray([
-        0x55,
-        0x00, 0x0A, 0x07, 0x01,
-        0xEB,
-        0xA5, 0x32, 0x20, 0x89, 0x00, 0xDE, 0xAD, 0xBE, 0xEF, 0x00,
-        0x03, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x00,
-        0x43
-    ])
-
-    # manually assemble packet
-    p = Packet(PACKET.RADIO)
-    p.rorg = RORG.BS4
-    sender_bytes = [(0xdeadbeef >> i & 0xff) for i in (24, 16, 8, 0)]
-    data = [0, 0, 0, 0]
-    p.data = [p.rorg] + data + sender_bytes + [0]
-
-    # test content
-    packet_serialized = p.build()
-    assert len(packet_serialized) == len(PACKET_CONTENT_1)
-    for i in range(len(packet_serialized)):
-        assert packet_serialized[i] == PACKET_CONTENT_1[i]
-
-    # set optional data
-    sub_tel_num = 3
-    destination = [255, 255, 255, 255]    # broadcast
-    dbm = 0xff
-    security = 0
-    p.optional = [sub_tel_num] + destination + [dbm] + [security]
-
-    # test content
-    packet_serialized = p.build()
-    assert len(packet_serialized) == len(PACKET_CONTENT_2)
-    for i in range(len(packet_serialized)):
-        assert packet_serialized[i] == PACKET_CONTENT_2[i]
-
-    # update data based on EEP
-    p.select_eep(0x20, 0x01)
-    prop = {
-        'CV': 50,
-        'TMP': 21.5,
-        'ES': 'true',
-    }
-    p.set_eep(prop)
-
-    # test content
-    packet_serialized = p.build()
-    assert len(packet_serialized) == len(PACKET_CONTENT_3)
-    for i in range(len(packet_serialized)):
-        assert packet_serialized[i] == PACKET_CONTENT_3[i]
-    assert p.rorg_func == 0x20
-    assert p.rorg_type == 0x01
