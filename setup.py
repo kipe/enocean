5550d547b5a9074e6e504f1ce4fb2fe8f616b6a5
Kimmo Huoman <kipenroskaposti@gmail.com>
2017-08-18 15:15:33 +0300

    Bump version to v0.40.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except ImportError:
 
 setup(
     name='enocean',
-    version='0.39',
+    version='0.40',
     description='EnOcean serial protocol implementation',
     author='Kimmo Huoman',
     author_email='kipenroskaposti@gmail.com',
