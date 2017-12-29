38d1852dbd2df28abaa5c82756f165523c643c70
Kimmo Huoman <kipenroskaposti@gmail.com>
2017-12-29 21:17:34 +0200

    Change to semantic versioning.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except ImportError:
 
 setup(
     name='enocean',
-    version='0.41',
+    version='0.41.0',
     description='EnOcean serial protocol implementation',
     author='Kimmo Huoman',
     author_email='kipenroskaposti@gmail.com',
